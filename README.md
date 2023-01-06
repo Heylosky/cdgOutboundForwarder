1e0f4b7943f2d0476b2b125a1d81d74cfbd2ec56
Heylosky <2547226479@qq.com>
2023-01-06 14:57:42 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+This is the part of CDG processor.
+This part will get message from message and send them to customer after filed replace. And push message which already sent to customer back to queue again, waiting DB record them.
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/cdgProcessor/outboundForwarder/logger"
+	"github.com/cdgProcessor/outboundForwarder/messageQ"
+	"github.com/cdgProcessor/outboundForwarder/models"
+	"go.uber.org/zap"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+)
+
+func main() {
+	logger.InitLogger("./outboundForwarder.log")
+	zap.L().Info("Outbound forwarder processor starting...")
+
+	waitSendChan := make(chan []byte)
+	sentChan := make(chan models.MbRc)
+
+	go messageQ.MQRead(waitSendChan, "outboundSMS", "outboundSmsToCu", "outboundToCu")
+	go messageQ.Publish(sentChan, "obSentRecordToDb")
+
+	Processor(waitSendChan, sentChan)
+}
+
+func Processor(c <-chan []byte, sentChan chan<- models.MbRc) {
+	var msg models.MbRc
+	client := &http.Client{}
+
+	for payload := range c {
+		err := json.Unmarshal(payload, &msg)
+		if err != nil {
+			zap.L().Error("json unmarshal sms error, message field is uncorrect.", zap.Error(err))
+		}
+		time.Sleep(2 * time.Millisecond) //simulate message field replace time
+		zap.L().Info("Replace some value in message", zap.Any("msg", msg))
+
+		urlMap := url.Values{}
+		urlMap.Add("payload", msg.Payload)
+
+		request, _ := http.NewRequest("POST", "http://127.0.0.1:8080/test", strings.NewReader(urlMap.Encode()))
+		request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+		res, err := client.Do(request)
+		if err != nil {
+			zap.L().Error("Client request err", zap.Error(err))
+		}
+		if res.StatusCode != 200 {
+			zap.L().Info("Failed to post message to customer api")
+		}
+		res.Body.Close()
+
+		sentChan <- msg
+	}
+}
